c913166b62d6b1fe3cf2e68cea15b4e5b364ca95
snehak2299 <skanase2299@gmail.com>
2021-08-25 15:37:15 +0530

    [sneha]:basic setup is added

---
+++ MyNode.java
@@ -0,0 +1,31 @@
+package com.bridglab.stack;
+
+public class MyNode<K>implements INode<K>{
+	private K key;
+	private INode next;
+	
+	public MyNode(K key) {
+		super();
+		this.key = key;
+		this.next = null;
+	}
+	public K getKey() {
+	    return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+		
+	}
+
+	public INode getNext() {
+		return next;
+	}
+
+	public void setNext(INode next) {
+		this.next = next;
+		
+	}
+	
+
+}
